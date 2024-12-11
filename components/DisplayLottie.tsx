b0e59ba861402eb88d3c566cb10bc77feeed1b17
AbedElRazzak <chreimabedelrazzak@gmail.com>
2024-12-11 14:09:25 +0200

    lottie server fix

--- components/DisplayLottie.tsx
+++ components/DisplayLottie.tsx
@@ -1,5 +1,32 @@
+// import React from "react";
+// import Lottie from "react-lottie";
+
+// type Props = {
+//   animationPath: string;
+// };
+
+// const GreetingLottie = ({ animationPath }: Props) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     path: animationPath,
+//   };
+
+//   return (
+//     <div onClick={() => null}>
+//       {/* @ts-ignore */}
+//       <Lottie options={defaultOptions} />
+//     </div>
+//   );
+// };
+
+// export default GreetingLottie;
+
+
 import React from "react";
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 type Props = {
   animationPath: string;
